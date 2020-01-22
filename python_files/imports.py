4db8e51eee3d08e02ac445bc15721446189cb573
Michael Pallante <michael.f.pallante@gmail.com>
2020-01-22 12:17:35 -0500

    updated notebooks

--- python_files/imports.py
+++ python_files/imports.py
@@ -12,11 +12,24 @@ import seaborn as sns
 
 from scipy import stats
 
+
+
 #  from IPython.display import display_html
+
+
 
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import RFE
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+
 
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
