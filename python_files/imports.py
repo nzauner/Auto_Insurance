0f55689d273c3a8e566ee36420049b9cf9a2c4f4
Michael Pallante <michael.f.pallante@gmail.com>
2020-01-22 15:38:50 -0500

    updated notebooks

--- python_files/imports.py
+++ python_files/imports.py
@@ -2,34 +2,28 @@
 
 import pandas as pd
 import numpy as np
-
-
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-
 from scipy import stats
-
-
-
-#  from IPython.display import display_html
-
-
 
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import RFE
+from sklearn.feature_selection import RFECV
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
+from sklearn.datasets import make_regression
+from sklearn import datasets, linear_model
 
-
+import warnings
+warnings.filterwarnings('ignore')
+pd.options.display.max_columns = None
 
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
