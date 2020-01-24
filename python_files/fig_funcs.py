5056ad9028660fad435567dd27811f219ca39a3a
nzauner <55512917+nzauner@users.noreply.github.com>
2020-01-24 13:19:39 -0500

    updated

--- python_files/fig_funcs.py
+++ python_files/fig_funcs.py
@@ -1,4 +1,4 @@
-def dist_plot_fig(df, col)
+def dist_plot_fig(df, col):
     '''Seaborn '''
     
     plt.figure(figsize=(10, 6))
@@ -7,19 +7,21 @@ def dist_plot_fig(df, col)
     #plt.xlim(left=)
     sns.despine()
     plt.show()
-    fig.savefig(f'../images/{col}_hist.png', transparent=True, dpi=2000, bbox_inches='tight')
+    fig.savefig(f'../visuals/{col}_hist.png', transparent=True, dpi=2000, bbox_inches='tight')
     
 
 def residuals_plot(filepfx):    
+    
     visualizer = ResidualsPlot(linreg_model)
     visualizer.fit(x_train_lin, y_train_lin)
     visualizer.score(x_test_lin, y_test_lin)  
     visualizer.show()                         
-    fig.savefig(f'../images/{filepfx}_residual_plot.png', transparent=True, dpi=2000, bbox_inches='tight')
+    fig.savefig(f'../visuals/{filepfx}_residual_plot.png', transparent=True, dpi=2000, bbox_inches='tight')
 
     
     
 def roc_curve_plot(filepfx):
+    
     logit_roc_auc = roc_auc_score(y_test_log, logreg.predict(x_test_log))
     fpr, tpr, thresholds = roc_curve(y_test_log, logreg.predict_proba(x_test_log)[:,1])
     plt.figure()
@@ -33,11 +35,12 @@ def roc_curve_plot(filepfx):
     plt.legend(loc="lower right")
     plt.savefig('Log_ROC')
     plt.show()
-    fig.savefig(f'../images/{filepfx}_roc_plot.png', transparent=True, dpi=2000, bbox_inches='tight')
+    fig.savefig(f'../visuals/{filepfx}_roc_plot.png', transparent=True, dpi=2000, bbox_inches='tight')
 
     
 
 def heat_map(filepfx):
+    
     mask = np.zeros_like(x_train_lin.corr())
     triangle_indices = np.triu_indices_from(mask)
     mask[triangle_indices] = True
@@ -52,4 +55,4 @@ def heat_map(filepfx):
     ax.set_ylim(bottom + 0.5, top - 0.5)
     #plt.ylabel(ylabel=' ', labelpad=100)
     plt.show()
-    fig.savefig(f'../images/{filepfx}_heat_map.png', transparent=True, dpi=2000, bbox_inches='tight')
+    fig.savefig(f'../visuals/{filepfx}_heat_map.png', transparent=True, dpi=2000, bbox_inches='tight')
